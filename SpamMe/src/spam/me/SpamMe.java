dacfb02ec7208535dfb5ab25893bee3c1834f2b5
Kim <kimmy@192.168.0.192>
2011-03-08 20:32:49 -0700

    Made UI changes to the main page.

--- SpamMe/src/spam/me/SpamMe.java
+++ SpamMe/src/spam/me/SpamMe.java
@@ -1,12 +1,15 @@
 package spam.me;
 
+
 import android.app.Activity;
+import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.provider.ContactsContract.CommonDataKinds.Phone;
 import android.provider.ContactsContract.PhoneLookup;
+import android.view.View;
 
 public class SpamMe extends Activity {
 	/** Called when the activity is first created. */
@@ -16,4 +19,11 @@ public class SpamMe extends Activity {
 		setContentView(R.layout.main);
 		
 	}
+    public void newGroupChatClicked(View v){
+    	int myReqCode = 1;
+    	//Intent featuredEvent = new Intent(v.getContext(), FeaturedEvent.class);
+    	//startActivityIfNeeded(featuredEvent, myReqCode);
+    	Intent newGroupChat = new Intent(v.getContext(), CreateGroupChatUI.class); 
+    	startActivityIfNeeded(newGroupChat, myReqCode); 
+    }
 }
